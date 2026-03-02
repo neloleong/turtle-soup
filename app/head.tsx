56a4f18ad69f878df0c77c9b555451eda93ee479
neloleong <neloleong@gmail.com>
2026-03-02 17:23:10 +0800

    update

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,11 @@
 export default function Head() {
   return (
     <>
+      <meta
+        name="google-site-verification"
+        content="l4jW3ViZI7ZQlkP5K4YAtFVIMbZ3fsVayoQGN7vcM_I"
+      />
+
       <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2071278885677120"
