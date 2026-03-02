226e2198d0253bf543590515572c5fa3e6126ab4
neloleong <neloleong@gmail.com>
2026-03-02 16:50:33 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 // /app/layout.tsx
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./ui/NavBar";
+
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -16,6 +18,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Script
+          id="adsense-loader"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2071278885677120"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         <NavBar />
         <main className="mx-auto max-w-5xl px-4 py-8">{children}</main>
       </body>
